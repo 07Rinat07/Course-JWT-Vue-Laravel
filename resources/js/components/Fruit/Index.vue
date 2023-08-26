e3319036ab9efebd8da7ecffc0eeb2003f978832
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-26 16:28:30 +0500

    create a project, set up a database, the basis for SPA, connect Vue Router

---
+++ resources/js/components/Fruit/Index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    List of fruits
+</div>
+
+</template>
+
+
+<script>
+export default {
+    name: "Index"
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <router-link :to="{name: 'fruit.index'}">List</router-link>
+    <router-view></router-view>
+</div>
+
+</template>
+
+
+<script>
+export default {
+    name: "Index"
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
