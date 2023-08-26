c89a5d00b2842e639267d2f0b2f72582b012c17b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-26 21:10:36 +0500

    Login and registration form in the SPA application. Getting a List of Fruits. Add database

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'User', 'prefix' => 'users'], function (){
+   Route::post('/', 'StoreController');
+});
+
+Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function (){
+    Route::get('/', 'IndexController');
+});
