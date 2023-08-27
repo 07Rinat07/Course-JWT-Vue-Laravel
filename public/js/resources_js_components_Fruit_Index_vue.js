3bbff07a5f4db8ad4a99a48b250dd2f15c8ae47e
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-27 22:36:21 +0500

    Get refresh token using axios request inside interceptors__5

--- public/js/resources_js_components_Fruit_Index_vue.js
+++ public/js/resources_js_components_Fruit_Index_vue.js
@@ -101,28 +101,39 @@ var api = axios__WEBPACK_IMPORTED_MODULE_0___default().create();
 //start request
 api.interceptors.request.use(function (config) {
   if (localStorage.getItem('access_token')) {
-    config.headers = {
-      'authorization': "Bearer ".concat(localStorage.getItem('access_token'))
-    };
+    config.headers.authorization = "Bearer ".concat(localStorage.getItem('access_token'));
   }
   return config;
 }, function (error) {});
 
 //end request
+// start response
 api.interceptors.response.use(function (config) {
   if (localStorage.getItem('access_token')) {
-    config.headers = {
-      'authorization': "Bearer ".concat(localStorage.getItem('access_token'))
-    };
+    config.headers.authorization = "Bearer ".concat(localStorage.getItem('access_token'));
   }
   return config;
 }, function (error) {
+  if (error.response.data.message === 'Token has expired') {
+    return axios__WEBPACK_IMPORTED_MODULE_0___default().post('api/auth/refresh', {}, {
+      headers: {
+        'authorization': "Bearer ".concat(localStorage.getItem('access_token'))
+      }
+    }).then(function (res) {
+      localStorage.setItem('access_token', res.data.access_token);
+      error.config.headers.authorization = "Bearer ".concat(res.data.access_token);
+      return api.request(error.config);
+    });
+  }
   if (error.response.status === 401) {
     _router__WEBPACK_IMPORTED_MODULE_1__["default"].push({
       name: 'user.login'
     });
   }
 });
+
+//end response
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (api);
 
 /***/ }),
